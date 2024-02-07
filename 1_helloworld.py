95bf3b6cb18c3b57cd5ca1b40c81008e3303972b
arounamounchili <virtual.power.virtual@gmail.com>
2024-02-07 20:41:57 +0100

    create file

--- 1_helloworld.py
+++ 1_helloworld.py
@@ -4,11 +4,3 @@ print("Welcome to this course.")
 # Ceci est un commentaire sur une ligne
 print("Bonjour Minteligencia!") # Encore un commentaire
 #print("On peut aussi commenter le code.")
-
-"""
-On peut utiliser ceci pour ecrire
-des commentaires sur plusieurs lignes
-ou commenter plusieurs lignes de code.
-"""
-
-# Indentation refers to the spaces at the beginning of a code line.
