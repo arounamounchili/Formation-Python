f616b2073867da311ac46d22028bf9dd990647b8
arounamounchili <virtual.power.virtual@gmail.com>
2024-02-07 20:35:44 +0100

    create file

---
+++ 1_helloworld.py
@@ -0,0 +1,14 @@
+print("Hello, World!")
+print("Welcome to this course.")
+
+# Ceci est un commentaire sur une ligne
+print("Bonjour Minteligencia!") # Encore un commentaire
+#print("On peut aussi commenter le code.")
+
+"""
+On peut utiliser ceci pour ecrire
+des commentaires sur plusieurs lignes
+ou commenter plusieurs lignes de code.
+"""
+
+# Indentation refers to the spaces at the beginning of a code line.
---
+++ 2_variable.py
@@ -0,0 +1,52 @@
+"""
+Les variables sont des conteneurs permettant de stocker des valeurs de données.
+Pour creer une variable on doit lui donner une nom et assigner une valeur.
+"""
+
+# Exemple
+x = 10
+y = 12.5
+nom = "Minteligencia"
+print(x)
+print(y)
+print(nom)
+
+# Comme dans d'autres langage de programmation, en Python on a pas besoin
+# de preciser le type de la variable. On peut aussir changer le type
+# d'une variable a tout moment.
+a = 45
+a = "Python"
+
+""" Connaitre le type d'une variable en utilisant la fonction 'type' """
+print(type(x))
+print(type(y))
+print(type(nom))
+
+
+""" Nom des Variables """
+# Une variable peut avoir un nom court comme x et y ou un nom plutot 
+# descriptive comme age, nom_de_famille.
+
+# C'est quoi la difference entre ces deux variables? -> une est plus descriptive
+age = 23
+x = 34
+
+# Les noms des variables sont sensibles a la casse
+prenom = "John"
+Prenom = "Fosso"
+print(prenom)
+print(Prenom)
+
+# Variables avec beaucoup de mots
+# - Camel Case
+tailleDePersonne = 1.75
+# - Pascal Case
+TailleDePersonne = 2.10
+# - Snake Case
+taille_de_personne = 1.75
+
+""" Exercice: Creer une variable appele nomDeSociete et donne lui la
+    la valeur 'Mercedes' et afficher le resultat.
+"""
+nomDeSociete = "Mercedes"
+print(nomDeSociete)
